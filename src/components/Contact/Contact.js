3a7b2661680f2c3940c668b94ddd7453938215cc
svetlanabaneva <svetlana.baneva@gmail.com>
2024-02-01 14:50:03 +0100

    confirm message

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,7 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import './contact.css'
 import cv from '../../assets/cv.pdf'
-//import * as Yup from 'yup'
 import emailjs from '@emailjs/browser';
-//import { useFormik } from "formik";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faSquarePhone } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -36,45 +34,30 @@ import {
         url: "https://facebook.com",
       },
   ];
-  /*---Yup Schema----*/
 
+const Result = () => {
+  return (
+    <p> Your message has been successfully sent. I will contact you soon</p>
+  )
+}
 
 const Contact = () => {
-
-  /*
-    const formik = useFormik({
-      initialValues: {
-        firstName:"",
-        email:"",
-        type:"",
-        comment:""
-      },
-      validationSchema: Yup.object({
-        name: Yup.string().required("Required"),
-        email: Yup.string().email("Invalid email").required("Required"),
-        message: Yup.string().min(25, "Must be at least 25 characters").required("Required")
-      }),
-    });*/
+  const [result, showResult] = useState(false)  
   /*------Sending Email with EmailJS-----------*/
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('', '', form.current, '')
+    emailjs.sendForm('service_5k8p3nc', 'template_xnkgd7d', form.current, 'Ih3Da1u9aHgUdik9Z')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+      e.target.reset();
+      showResult(true);
     };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //formValidation();
-    sendEmail();
-
-  }
 
   return (
     <section id='contact' >
@@ -96,13 +79,14 @@ const Contact = () => {
             <div><a href={cv} download className='cvBtn'>Download CV</a></div>
           </div>
           <div className='contact-right'>
-            <form className='contactForm' ref={form} onSubmit={handleSubmit}>
+            <form className='contactForm' ref={form} onSubmit={sendEmail}>
               <p className='contactDesc'>Please fill out the form to discuss any work opportunities</p>
               <input type='text' className='name' placeholder='Your Name' name="from_name" required />
               <input type='email' className='email' placeholder='Your Email' name='from_email' required />
               <textarea name='message' rows='5' placeholder='Your Message' className='msg' required/>
               <div><button type='submit' className='submitBtn' value='Sent'>Submit</button></div>
             </form>
+            <div className='confirm'>{result ? <Result /> : null}</div>
           </div>
         </div>
 
