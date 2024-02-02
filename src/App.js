a50e5aa32e841fcf1228a025b99712cd8c071e3d
svetlanabaneva <svetlana.baneva@gmail.com>
2024-02-02 16:58:15 +0100

    animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Contact from "./components/Contact/Contact";
+//import Test from "./components/Test/Test";
 import Footer from "./components/Footer/Footer";
 import Intro from "./components/Intro/Intro";
 import NavBar from "./components/NavBar/NavBar";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
-
 
 function App() {
   return (
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,14 +1,62 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import './intro.css'
 
 import profile from '../../assets/profile.JPG'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 
 const Intro = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = [' ', 'S', 'v', 'e', 't', 'l', 'a', 'n', 'a']
+  const jobArray = [
+    'F',
+    'r',
+    'o',
+    'n',
+    't',
+    'e',
+    'n',
+    'd',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000);
+    return () => clearTimeout(timer);
+}, [])
   return (
    <section id='intro'>
     <div className='introContent'>
-        <span className='hello'>Hi,</span>
-        <span className='introText'>I'm <span className='introName'>Svetlana</span><br />Frontend Developer</span>
+        <span className='introText'>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i, </span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m </span>
+            <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+            />
+        <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+        </span>
         <p className='introPara'>I am a motivated and self-driven front end developer, re-entering the field. <br />After a long break invested in my family I am ready for new challenges.<br />Having spent more than an year in training on the newest technologies<br /> I am eager to get my hands on real projects.</p>
     </div>
     <img src={profile} alt="Profile" className='bg' />
